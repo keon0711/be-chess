fa8f533854f94cd1619e4851364388190672beea
Keon Kim <fnc6488@gmail.com>
2024-03-04 18:21:58 +0900

    보드 테스트 코드 리팩토링
    보드에 폰을 추가하는 기능의 테스트 코드에서 중복을 제거

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -1,24 +1,33 @@
 package chess;
 
 import chess.pieces.Pawn;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BoardTest {
 
+    static Board board;
+
+    @BeforeAll
+    static void init() {
+        board = new Board();
+    }
+
     @Test
+    @DisplayName("보드에 폰을 추가할 수 있다")
     void create() {
-        Board board = new Board();
+        verifyAdd(new Pawn(Pawn.WHITE_COLOR), 1);
+        verifyAdd(new Pawn(Pawn.BLACK_COLOR), 2);
+    }
 
-        Pawn white = new Pawn(Pawn.WHITE_COLOR);
-        board.add(white);
-        assertThat(board.size()).isEqualTo(1);
-        assertThat(board.findPawn(0)).isEqualTo(white);
 
-        Pawn black = new Pawn(Pawn.WHITE_COLOR);
-        board.add(black);
-        assertThat(board.size()).isEqualTo(2);
-        assertThat(board.findPawn(1)).isEqualTo(black);
+    private void verifyAdd(Pawn pawn, int expectedSize) {
+        board.add(pawn);
+
+        assertThat(board.size()).isEqualTo(expectedSize);
+        assertThat(board.findPawn(expectedSize - 1)).isEqualTo(pawn);
     }
 }
