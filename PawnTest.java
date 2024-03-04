92ef99eb15d8320e31096e75d9f381a3e07bba9e
Keon Kim <fnc6488@gmail.com>
2024-03-04 13:43:08 +0900

    Pawn 생성 기능 구현

--- PawnTest.java
+++ PawnTest.java
@@ -3,10 +3,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PawnTest {
+
     @Test
     @DisplayName("흰색 폰이 생성되어야 한다")
     void create() {
-        Pawn pawn = new Pawn("white");
-        Assertions.assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        Assertions.assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        Assertions.assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
