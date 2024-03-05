5251c87ad1f0bde95d768a4607bec6ff434123fd
Keon Kim <fnc6488@gmail.com>
2024-03-05 19:20:42 +0900

    Piece의 검은말, 흰말을 구분하는 기능 구현
    - 검은말, 흰말을 구분할 수 있는 `isBlack()`, `isWhite()` 메서드 구현
    - 같은색 기물을 담은 리스트에 allMatch(Piece::isBlack)으로 색깔을 검증

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -84,4 +84,12 @@ public class Piece {
     public char getRepresentation() {
         return representation;
     }
+
+    public boolean isBlack() {
+        return color == Color.BLACK;
+    }
+
+    public boolean isWhite() {
+        return color == Color.WHITE;
+    }
 }
--- src/test/java/chess/pieces/PieceTest.java
+++ src/test/java/chess/pieces/PieceTest.java
@@ -1,6 +1,10 @@
 package chess.pieces;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -54,6 +58,34 @@ class PieceTest {
     @Test
     void createWhiteKing() {
         verifyPiece(Piece.createWhiteKing(), Color.WHITE, Piece.WHITE_KING_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("검은색 기물들은 모두 검은색이어야 함")
+    void isBlackColor() {
+        List<Piece> pieces = new ArrayList<>();
+        pieces.add(Piece.createBlackKing());
+        pieces.add(Piece.createBlackQueen());
+        pieces.add(Piece.createBlackBishop());
+        pieces.add(Piece.createBlackKnight());
+        pieces.add(Piece.createBlackRook());
+        pieces.add(Piece.createBlackPawn());
+
+        assertThat(pieces).allMatch(Piece::isBlack);
+    }
+
+    @Test
+    @DisplayName("흰색 기물들은 모두 흰색이어야 함")
+    void isWhiteColor() {
+        List<Piece> pieces = new ArrayList<>();
+        pieces.add(Piece.createWhiteKing());
+        pieces.add(Piece.createWhiteQueen());
+        pieces.add(Piece.createWhiteBishop());
+        pieces.add(Piece.createWhiteKnight());
+        pieces.add(Piece.createWhiteRook());
+        pieces.add(Piece.createWhitePawn());
+
+        assertThat(pieces).allMatch(Piece::isWhite);
     }
 
     private void verifyPiece(final Piece piece, final Color color, final char representation) {
