428f80cc340c6da6cee549cbab5bab6872b34937
Keon Kim <fnc6488@gmail.com>
2024-03-05 18:22:33 +0900

    Piece 클래스 메서드 시그니처 변경
    Piece 객체를 생성하는 생성자, 팩토리 메서드 인자 순서를 보기쉽게 type이 color보다 먼저 오도록 수정

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -5,27 +5,27 @@ public class Piece {
     public static final char BLACK_REPRESENTATION = 'P';
     public static final char WHITE_REPRESENTATION = 'p';
 
-    private final Color color;
     private final Type type;
+    private final Color color;
     private final char representation;
 
-    private Piece(Color color, Type type, char representation) {
+    private Piece(Type type, Color color, char representation) {
         this.color = color;
         this.type = type;
         this.representation = representation;
     }
 
-    public static Piece of(Color color, Type name) {
+    public static Piece of(Type type, Color color) {
         char representation = color.equals(Color.BLACK) ? BLACK_REPRESENTATION : WHITE_REPRESENTATION;
-        return new Piece(color, name, representation);
+        return new Piece(type, color, representation);
     }
 
     public static Piece createWhitePawn() {
-        return of(Color.WHITE, Type.PAWN);
+        return of(Type.PAWN, Color.WHITE);
     }
 
     public static Piece createBlackPawn() {
-        return of(Color.BLACK, Type.PAWN);
+        return of(Type.PAWN, Color.BLACK);
     }
 
     public Color getColor() {
