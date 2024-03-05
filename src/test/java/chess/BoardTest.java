2330cc89c147e51e45d49a7286723e9cd8d1f1ab
Keon Kim <fnc6488@gmail.com>
2024-03-05 15:13:26 +0900

    Board 초기화 메서드 구현
    Board를 초기화하면 흰색, 검은색 폰 객체가 8씩 저장되어야 한다.

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -20,16 +20,28 @@ public class BoardTest {
     @Test
     @DisplayName("보드에 폰을 추가할 수 있다")
     void create() {
-        verifyAdd(new Pawn(Color.WHITE), 1);
-        verifyAdd(new Pawn(Color.BLACK), 2);
+        Pawn white = new Pawn(Color.WHITE, Pawn.WHITE_REPRESENTATION);
+        board.addWhite(white);
+        assertThat(board.size()).isEqualTo(1);
+        assertThat(board.findWhitePawn(0)).isEqualTo(white);
+
+        Pawn black = new Pawn(Color.BLACK, Pawn.BLACK_REPRESENTATION);
+        board.addBlack(black);
+        assertThat(board.size()).isEqualTo(2);
+        assertThat(board.findBlackPawn(0)).isEqualTo(black);
     }
 
+    @Test
+    @DisplayName("보드를 초기화하면 검은색 Pawn이 생성되어야 한다.")
+    void initBlackPawns() {
+        board.initialize();
+        assertThat(board.getBlackPawnsResult()).isEqualTo("PPPPPPPP");
+    }
 
-
-    private void verifyAdd(Pawn pawn, int expectedSize) {
-        board.add(pawn);
-
-        assertThat(board.size()).isEqualTo(expectedSize);
-        assertThat(board.findPawn(expectedSize - 1)).isEqualTo(pawn);
+    @Test
+    @DisplayName("보드를 초기화하면 흰색 Pawn이 생성되어야 한다.")
+    void initWhitePawns() {
+        board.initialize();
+        assertThat(board.getWhitePawnsResult()).isEqualTo("pppppppp");
     }
 }
