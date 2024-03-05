75a56e75dc8618dac606419afd4d7728f70bb1b0
Keon Kim <fnc6488@gmail.com>
2024-03-05 12:21:15 +0900

    build.gradle 에서 Junit 5 종속성 변경
    parameterizedTest 메서드 사용을 위해 build.gradle 파일의 dependencies 에서 junit-jupiter-api에서 junit-jupiter 변경

--- src/test/java/chess/BoardTest.java
+++ src/test/java/chess/BoardTest.java
@@ -6,10 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Method;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class BoardTest {
 
