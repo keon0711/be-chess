5f42879ac15c2a5f2b00d9d8a72ab319d8212e56
Keon Kim <fnc6488@gmail.com>
2024-03-07 14:22:24 +0900

    체스판 좌표 범위를 제한하는 로직 추가
    체스판 좌표가 a~h, 1~8 사이가 아닐 경우 예외를 발생시키는 검증 메서드 구현

--- src/main/java/chess/Position.java
+++ src/main/java/chess/Position.java
@@ -2,19 +2,34 @@ package chess;
 
 public record Position(int row, int column) {
 
-    public static final int COLUMN_INDEX = 0;
-    public static final int ROW_INDEX = 1;
+    public static final int FILE_INDEX = 0;
+    public static final int RANK_INDEX = 1;
+    public static final char MIN_FILE = 'a';
+    public static final char MAX_FILE = 'h';
+    public static final char MIN_RANK = '1';
+    public static final char MAX_RANK = '8';
 
     public static Position of(String position) {
         return new Position(calcRow(position), calcColumn(position));
     }
 
     private static int calcRow(String position) {
-        return Rank.MAX_RANK_SIZE - Character.getNumericValue(position.charAt(ROW_INDEX));
+        validate(position);
+        return Rank.MAX_RANK_SIZE - Character.getNumericValue(position.charAt(RANK_INDEX));
     }
 
     private static int calcColumn(String position) {
-        return position.charAt(COLUMN_INDEX) - 'a';
+            return position.charAt(FILE_INDEX) - MIN_FILE;
+    }
+
+
+    private static void validate(String position) {
+        if (position.charAt(FILE_INDEX) < MIN_FILE || position.charAt(FILE_INDEX) > MAX_FILE) {
+            throw new IllegalArgumentException("체스판의 랭크 좌표는 a~h 여야합니다.");
+        }
+        if (position.charAt(RANK_INDEX) < MIN_RANK || position.charAt(RANK_INDEX) > MAX_RANK) {
+            throw new IllegalArgumentException("체스판의 파일 좌표는 1~8 여야합니다.");
+        }
     }
 
 }
--- src/test/java/chess/PositionTest.java
+++ src/test/java/chess/PositionTest.java
@@ -1,25 +1,42 @@
 package chess;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PositionTest {
 
     @Test
     @DisplayName("g1은 7행 6열이어야 한다.")
-    void verifyPosition1() {
+    void g1_should_be_pared_7_row_6_column() {
         Position pos = Position.of("g1");
 
-        Assertions.assertThat(pos).isEqualTo(new Position(7, 6));
+        assertThat(pos).isEqualTo(new Position(7, 6));
     }
 
     @Test
     @DisplayName("a8은 0행 0열이어야 한다.")
-    void verifyPosition2() {
+    void a8_should_be_pared_0_row_0_column() {
         Position pos = Position.of("a8");
 
-        Assertions.assertThat(pos).isEqualTo(new Position(0, 0));
+        assertThat(pos).isEqualTo(new Position(0, 0));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"a0, a9, c0, c9, g0, g9, h0, h9"})
+    @DisplayName("Rank가 1~8 사이가 아니면 예외를 발생시켜야 한다.")
+    void rankIndex_ShouldBe_Between_One_And_Eight(String position) {
+        assertThatThrownBy(() -> Position.of(position)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a0, a9, c0, c9, g0, g9, h0, h9"})
+    @DisplayName("File이 a~h 사이가 아니면 예외를 발생시켜야 한다.")
+    void fileIndex_ShouldBe_Between_A_And_H(String position) {
+        assertThatThrownBy(() -> Position.of(position)).isInstanceOf(IllegalArgumentException.class);
+    }
 }
